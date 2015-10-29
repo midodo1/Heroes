f87facd20e25a851d53ad7e91eed6f7872a27303
mehdi <mehdi.nacer@ensea.fr>
2015-10-29 20:46:19 +0100

    commit

---
+++ build/CMakeFiles/executable-client.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/executable-client.dir/src/main.cpp.o"
+  "../bin/executable-client.pdb"
+  "../bin/executable-client"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/executable-client.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
